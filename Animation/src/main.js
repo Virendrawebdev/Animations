01cfc2dacfd738268c14cbaad29ec9b444df9376
Virendrawebdev <virendra052001@gmail.com>
2026-06-13 13:37:59 +0530

    Day04 Animation Timeline control methods master Timeline

--- Animation/src/main.js
+++ Animation/src/main.js
@@ -5,17 +5,14 @@ import './style.css'
 
 // const box = document.querySelector('.box')
 // gsap.to(['.box', '.box2'])
-
 //to method is used to animate from a starting point to an ending point
 //from method is used to animate from an ending point to a starting point
-
 // gsap.from('.box',{
 //     delay:0.6,
 //     x:500,
 //     duration:1,
 //     ease:'bounce'
 // })
-
 //fromTo method is used to animate from a starting point to an ending point and then back to the starting point
 // gsap.fromTo('.box', {
 //     x: 0,
@@ -48,11 +45,9 @@ import './style.css'
 //     repeat:3,
 //     yoyo:true
 // })
-
 // gsap.set('.box',{
 //     x:-300,
 // })
-
 // gsap.from('.box',{
 //     delay:0.6,
 //     x:1500,
@@ -61,9 +56,7 @@ import './style.css'
 //     repeat:-1,
 // })
 
-
 // callback function after animation is complete
-
 // gsap.to('.box', {
 //     x:500,
 //     duration:1,
@@ -71,7 +64,6 @@ import './style.css'
 //         console.log('Animation complete');
 //     }
 // })
-
 // gsap.to('.box', {
 //     x:500,
 //     duration:1,
@@ -113,25 +105,25 @@ import './style.css'
 // timeline
 
 
-const tl = gsap.timeline();
-tl.to('.box',{
-    x:1000,
-    duration:1,
-    ease:'power2.inOut',
-    delay:0.6,
-}).to('.box1',{
-    x:1000,
-    duration:1,
-    ease:'power2.inOut',
-},"viru",).to('.box2',{
-    x:1000,
-    duration:1,
-    ease:'power2.inOut',
-},"viru-=0.5",).to('.box3',{
-    x:1000,
-    duration:1,
-    ease:'power2.inOut',
-})
+// const tl = gsap.timeline();
+// tl.to('.box',{
+//     x:1000,
+//     duration:1,
+//     ease:'power2.inOut',
+//     delay:0.6,
+// }).to('.box1',{
+//     x:1000,
+//     duration:1,
+//     ease:'power2.inOut',
+// },"viru",).to('.box2',{
+//     x:1000,
+//     duration:1,
+//     ease:'power2.inOut',
+// },"viru-=0.5",).to('.box3',{
+//     x:1000,
+//     duration:1,
+//     ease:'power2.inOut',
+// })
 
 
 
@@ -142,3 +134,80 @@ tl.to('.box',{
 //1."<" => a. "<0.2"
 //2. "=" => a. "=-0.2"
 //3. labels 
+
+// Day04_ANIMATION
+// Timeline control methods
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+const reverse = document.querySelector('.reverse');
+const restart = document.querySelector('.restart');
+const resume = document.querySelector('.resume');
+const seek = document.querySelector('.seek');
+
+const tl = gsap.timeline({paused:true});
+
+tl.to('.box',{
+    x:1000,
+    duration:1,
+    ease:'power2.inOut',
+    delay:0.6,
+}).to('.box1',{
+    x:1000,
+    duration:1,
+    ease:'power2.inOut',
+})
+.addLabel("viru")
+.to('.box2',{
+    x:1000,
+    duration:1,
+    ease:'power2.inOut',
+})
+.to('.box3',{
+    x:1000,
+    duration:1,
+    ease:'power2.inOut',
+})
+
+play.addEventListener('click',()=>{
+    tl.play();
+})
+pause.addEventListener('click',()=>{
+    tl.pause();
+})
+reverse.addEventListener('click',()=>{
+    tl.reverse();
+})
+restart.addEventListener('click',()=>{
+    tl.restart();
+})
+resume.addEventListener('click',()=>{
+    tl.resume();
+}
+)
+seek.addEventListener('click',()=>{
+    tl.seek("viru");
+})
+
+//Architecture of GSAP Layer
+
+//Loading Timeline
+
+// const loadingTimeline =()=>{
+//     return gsap.timeline().to(Element, {})
+// }
+
+// // Navbar Timeline
+// const navbarTimeline =()=>{
+//     return gsap.timeline().to(Element, {})
+// }
+
+// // Master Timeline
+
+// const master = gsap.timeline();
+
+// master.add(loadingTimeline()).add(navbarTimeline(),"-=0.5")
+
+
+
+
+
